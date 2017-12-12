0191cd7c5e81b764772e26e891fb67cfccea62f0
disi <disi@DESKTOP-OKQ3QAL>
2017-12-12 19:23:00 +0000

    working code for w_info.hdr

--- ElexModMerger/ElexModMerger.cs
+++ ElexModMerger/ElexModMerger.cs
@@ -12,63 +12,85 @@ namespace ElexModMerger
     {
         static void Main(string[] args)
         {
+            // get the current directory
+            string workingDir = @System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
             // extract all them mod files
             if (File.Exists("m_9_MergeMod.pak"))
             {
-                Process.Start("CMD.exe", "/C echo \"*\" | elexresman.exe " + "m_9_MergeMod.pak");
+                Process.Start("CMD.exe", "/C echo \"*\" | elexresman.exe " + "m_9_MergeMod.pak").WaitForExit();
             }
             var mods = Directory.GetFiles(".", "m_?_*.pak");
             foreach (string file in mods)
             {
-                Process.Start("CMD.exe", "/C echo \"*\" | elexresman.exe " + file);
+                Process.Start("CMD.exe", "/C echo \"*\" | elexresman.exe " + file).WaitForExit();
             }
-            // here needs to be an input to wait for the files to write to disk
-            Console.WriteLine("Extracted all mods!");
+            Console.WriteLine("Extracted all mods");
+            Console.WriteLine("Press any key!");
             Console.ReadKey();
 
-            // find all them w_info.hdr and convert those, then add to winforesult
-            string path = @System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var wInfofiles = Directory.GetFiles(path, "w_info.hdr", SearchOption.AllDirectories);
-            Console.WriteLine("Found the w_info.hdr files in " + path + " \nNumber of files: " + wInfofiles.Length);
-            Console.ReadKey();
-            Dictionary<string, List<string>> wInfoResult = new Dictionary<string, List<string>>();
-            foreach (string winfofile in wInfofiles)
+            // find all them w_info.hdr and convert those, then add to winforesult and write new w_info.hdr
+            var wInfofiles = Directory.GetFiles(workingDir, "w_info.hdr", SearchOption.AllDirectories);
+            if (wInfofiles != null)
             {
-                Console.WriteLine("File : " + winfofile);
-                Console.ReadKey();
-                Process.Start("CMD.exe", "/C elexresman.exe " + winfofile);
-                // here needs to be an input to wait for the file to write to disk or maybe some flush-command?
-                var wInfo = readInfos(infofile: winfofile + "doc");
-                wInfoResult.Concat(wInfo.Where(x => !wInfoResult.Keys.Contains(x.Key)));
-            }
-
-            /*
-            Dictionary<string, List<string>> wInfo1 = readInfos(infofile: "w_info.hdrdoc");
-            Dictionary<string, List<string>> wInfo2 = readInfos(infofile: "w_info1.hdrdoc");
-            Console.WriteLine("Read files!");
-            Console.WriteLine("Press any key to combine!");
-            Console.ReadKey();
-            var winforesult = wInfo1.Concat(wInfo2.Where(x => !wInfo1.Keys.Contains(x.Key))); ;
-            Console.WriteLine("Combined!");
-            Console.WriteLine("Press any key to write!");
-            Console.ReadKey();
-            TextWriter tw = new StreamWriter("w_info_new.hdrdoc");
-            tw.WriteLine("class gCInfo {");
-            foreach (KeyValuePair<string, List<string>> info in winforesult)
-            {
-                foreach (string item in info.Value)
+                Dictionary<string, List<string>> wInfoResult = new Dictionary<string, List<string>>();
+                foreach (string winfofile in wInfofiles)
                 {
-                    tw.WriteLine(item);
+                    Process.Start("CMD.exe", "/C elexresman.exe " + winfofile).WaitForExit();
+                    Console.WriteLine("Adding current File: " + winfofile);
+                    var wInfo = ReadInfos(infofile: winfofile + "doc");
+                    foreach (KeyValuePair<string, List<string>> info in wInfo)
+                    {
+                        if (!wInfoResult.ContainsKey(info.Key))
+                        {
+                            wInfoResult.Add(info.Key, info.Value);
+                        }
+                    }
                 }
+                if (wInfoResult != null)
+                {
+                    Directory.CreateDirectory(workingDir + "\\MergeMod\\documents\\");
+                    TextWriter tw = new StreamWriter(workingDir + "\\MergeMod\\documents\\w_info.hdrdoc");
+                    tw.WriteLine("class gCInfo {");
+                    foreach (KeyValuePair<string, List<string>> info in wInfoResult)
+                    {
+                        foreach (string item in info.Value)
+                        {
+                            tw.WriteLine(item);
+                        }
+                    }
+                    tw.WriteLine("}");
+                    tw.Close();
+                }
+                Process.Start("CMD.exe", "/C elexresman.exe " + workingDir + "\\MergeMod\\documents\\w_info.hdrdoc").WaitForExit();
+                File.Delete(workingDir + "\\MergeMod\\documents\\w_info.hdrdoc");
             }
-            tw.WriteLine("}");
-            tw.Close();
-            Console.WriteLine("Done!");
+
+            // create MergeMod
+            Console.WriteLine("Create m_9_MergeMod.pak");
+            Console.WriteLine("Press any key!");
             Console.ReadKey();
-            */
+
+            System.Diagnostics.Process myProcess = new System.Diagnostics.Process();
+            myProcess.StartInfo.FileName = "elexresman.exe";
+            myProcess.StartInfo.Arguments = workingDir + "\\MergeMod";
+            myProcess.StartInfo.UseShellExecute = false;
+            myProcess.StartInfo.RedirectStandardInput = true;
+            myProcess.Start();
+            System.IO.StreamWriter myStreamWriter = myProcess.StandardInput;
+            myStreamWriter.WriteLine("9");
+            myProcess.WaitForExit();
+
+            // cleanup
+            CleanDirs(workingDir);
+
+            // done
+            Console.WriteLine("");
+            Console.WriteLine("All done!");
+            Console.ReadKey();
         }
 
-        private static Dictionary<string, List<string>> readInfos(string infofile)
+        private static Dictionary<string, List<string>> ReadInfos(string infofile)
         {
             Dictionary<string, List<string>> newDict = new Dictionary<string, List<string>>();
             string[] lines = File.ReadAllLines(infofile);
@@ -91,6 +113,19 @@ namespace ElexModMerger
                 }
             }
             return newDict;
+        }
+
+        private static void CleanDirs(string moddir)
+        {
+            Console.WriteLine("Press 'y' to remove all directories!");
+            ConsoleKeyInfo result = Console.ReadKey();
+            if (result.KeyChar == 'y' || result.KeyChar == 'Y')
+            {
+                foreach (var subDir in new DirectoryInfo(@moddir).GetDirectories())
+                {
+                    subDir.Delete(true);
+                }
+            }
         }
     }
 }
